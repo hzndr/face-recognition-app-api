fc0cc792d487863e715c2da5de1d6aabd8ff8195
hzndr <hania.zander@gmail.com>
2021-05-08 18:48:51 +0200

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-rigid-04311",
     user: "postgres",
     password: "hania",
     database: "face_recognition_app",
